495454362912ce06b0186c05023cda423db8095d
Chin Kuay <kuaychin@gmail.com>
2020-03-25 20:00:47 -0500

    modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-import Todo from './Todo';
+import Todo from "./Todo";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
 
