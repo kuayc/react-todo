426d2afca6c31ace43ac83963617c13f47a15e2d
Chin Kuay <kuaychin@gmail.com>
2020-03-25 20:40:50 -0500

    modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
-
 import Todo from "./Todo";
 
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       show: false
     };
@@ -15,6 +13,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>Things to do in Life</h1>
+        <h2>(a.k.a. Bucket List)</h2>
         <Todo />
       </div>
     );
--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 
 class Todo extends Component {
-
   constructor(props) {
     super(props);
-
     this.state = {
       edit: false,
       id: null,
